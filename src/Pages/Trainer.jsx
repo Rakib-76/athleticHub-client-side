fa9e01819d7176274ecc7f7272e733254eb493eb
Rakib-76 <mrrakibhasan76@gmail.com>
2025-06-13 00:33:31 +0600

    added the forth commit

--- src/Pages/Trainer.jsx
+++ src/Pages/Trainer.jsx
@@ -15,11 +15,9 @@ const Trainer = () => {
                             className="rounded-xl" />
                     </figure>
                     <div className="card-body items-center text-center">
-                        <h2 className="card-title">Card Title</h2>
-                        <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
+                        <h2 className="card-title">Ben Nebert</h2>
+                        <p>Professional Trainer</p>
+                        
                     </div>
                 </div>
                 <div className="card bg-base-100 w-96 shadow-sm">
@@ -55,7 +53,7 @@ const Trainer = () => {
                 <div className="card bg-base-100 w-96 shadow-sm">
                     <figure className="px-10 pt-10">
                         <img
-                            src="https://i.postimg.cc/MZc8VDMW/baseball-player-posing-while-holding-bat.jpg"
+                            src="https://i.postimg.cc/mDmX50PP/spencer-davis-0-Sh-Ts8i-PY28-unsplash.jpg"
                             alt="Shoes"
                             className="rounded-xl" />
                     </figure>
